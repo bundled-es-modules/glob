cbe037e2b92c3c625600a84ce5b3716ef867cc85
jorenbroekema <joren.broekema@gmail.com>
2024-06-28 11:37:48 +0200

    fix: apply glob types files, upgrade stream/deps

--- tests/node-custom.js
+++ tests/node-custom.js
@@ -7,6 +7,6 @@ fs.writeFileSync(`tests/hello.txt`, "Hello!", "utf-8");
 fs.writeFileSync(`tests/hello2.txt`, "Hello!", "utf-8");
 
 console.assert(
-  JSON.stringify(glob.sync("tests/*.txt", { fs })) ===
+  JSON.stringify(glob.sync("/tests/*.txt", { fs })) ===
     JSON.stringify(["/tests/hello2.txt", "/tests/hello.txt"])
 );
